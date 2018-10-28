713f9899b07162de6b365c75a44313bd08a17041
Gordon <943215724@qq.com>
2018-10-28 17:07:34 +0800

    详情页制作

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    const mySwiper = new Swiper ('.headSwiper .swiper-container', {
+      loop: true, // 循环模式选项
+      autoplay: true,
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+    });
   }
 
 }
