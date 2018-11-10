1eee2c87628d97b14094871285ccc6a1e3fb973f
Gordon <943215724@qq.com>
2018-11-10 11:38:27 +0800

    导师首页

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher.component.css']
 })
 export class TeacherComponent implements OnInit {
-
+  showQr = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getQrFn() {
+    this.showQr = true;
   }
 
 }
