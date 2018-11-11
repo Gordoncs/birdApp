5f0ff468bc63ac49f67e229a01e024d6227b694d
太阳 <943215724@qq.com>
2018-11-11 08:19:41 +0800

    上传转file

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -29,11 +29,13 @@ export class UploadComponent implements OnInit {
   sureImg() {
     // @ts-ignore
     const cas = this.cropperArr[this.indexNow].obj.getCroppedCanvas();
-    console.log(cas);
     const base64 = cas.toDataURL('image/jpeg'); // 转换为base64
-    console.log(this.tobeFile(base64));
     const data = encodeURIComponent(base64);
-    console.log(this.tobeFile(data));
+    console.log(111, base64)
+    console.log(222, data)
+    console.log(this.dataURLtoFile(base64, 'bbq'));
+    // console.log(this.tobeFile(data));
+    // this.tobeFile(base64);
     this.cropperArr[this.indexNow].returnData = data;
     this.indexNow = -1;
     this.isshow = false;
@@ -59,24 +61,15 @@ export class UploadComponent implements OnInit {
   save() {
     console.log(this.cropperArr);
   }
-  convertBase64UrlToBlob(urlData) {
-    const bytes = window.atob(urlData.split(',')[1]);        // 去掉url的头，并转换为byte
 
-    // 处理异常,将ascii码小于0的转换为大于0
-    const ab = new ArrayBuffer(bytes.length);
-
-    const ia = new Uint8Array(ab);
-
-    for (let i = 0; i < bytes.length; i++) {          ia[i] = bytes.charCodeAt(i);      }
-    console.log(1111, new Blob( [ab] , {type : 'image/png'}));
-    return new Blob( [ab] , {type : 'image/png'});
-
-  }
-  tobeFile(base64Codes) {
-    const t = this;
-    const formData = new FormData();
-    // convertBase64UrlToBlob函数是将base64编码转换为Blob
-    formData.append('imageName', t.convertBase64UrlToBlob(base64Codes), 'aaaa');  // append函数的第一个参数是后台获取数据的参数名,和html标签的input的name属性功能相同
-    console.log(22222, formData);
+  dataURLtoFile(dataurl, filename) {// 将base64转换为文件
+    const arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]);
+    let n = bstr.length;
+    const  u8arr = new Uint8Array(n);
+    while ( n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type: mime});
   }
 }
